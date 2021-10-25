b90f12c72856acb096231f35e0cc0e4f8c7765e9
RizalMadani <rizalmadani70@gmail.com>
2021-10-25 14:53:53 +0700

    Clear unwanted comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-// const image_project_translate = 'lg:-translate-y-16';
+window.addEventListener('load', () => {
+    // fade anim delay + fade anim duration + threshold = 4.2
+    let time_out = (2.4 + 0.8 + 1) * 1000;
+
+    setTimeout(() => {
+        let cover = document.getElementById('cover');
+
+        cover.style.display = 'none';
+    }, time_out);
+});
+
 const project_cards = document.querySelectorAll('.project-card');
 const btn_back = document.getElementById('btn-back');
 let prodesc_target = -1;
@@ -14,7 +24,6 @@ project_cards.forEach((project_card, i) => {
     project_card.addEventListener('click', openProDesc);
 });
 
-// btn_back.onclick = openProDesc();
 btn_back.addEventListener('click', openProDesc);
 
 function openProDesc() {
@@ -34,45 +43,13 @@ document.getElementById('this-specific').onclick = () => {
     window.location.href = 'm'+srl+'m';
 }
 
-
-function transition1(project_card, set) {
-    let top, left;
-
-    if (set) {
-        let pos = project_card.getBoundingClientRect();
-
-        top = pos.top+'px';
-        left = pos.left+'px';
-    }
-    else {
-        top = '0';
-        left = '0';
-    }
-
-    // let pos = project_card.getBoundingClientRect();
-
-    //     top = pos.top+'px';
-    //     left = pos.left+'px';
-
-    console.log({'top': top, 'left': left});
-
-    let project_description = document.getElementById('prodesc-1');
-
-    project_description.style.top = top;
-    project_description.style.left = left;
-}
-
-
 const toggler = document.getElementById('nav-toggler');
 
 toggler.onclick = () => {
-    // let menu = document.getElementById('menu-nav');
     const header = document.querySelector('header');
     
     header.classList.toggle('h-16');
     header.classList.toggle('open-nav');
-    // header.classList.toggle('h-80');
-    // header.classList.toggle('shadow-header');
 
     const child = toggler.children[0];
 
